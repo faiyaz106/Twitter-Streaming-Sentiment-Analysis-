710e6eb42e91d575534c40ab19a7136d24db95d1
Faiyaz Ahmad <faiyazahmed106@gmail.com>
2023-12-20 12:10:44 -0500

    Updated code

--- streaming.py
+++ streaming.py
@@ -1,63 +1,40 @@
 import findspark
-findspark.init('/etc/spark')
-import pyspark
-from pyspark import RDD
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
+findspark.init()
+
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 from pyspark.ml.classification import DecisionTreeClassificationModel
-from pyspark.ml.feature import Word2VecModel
-
-# Need to start spark context first
-sc=SparkContext(appName="Kafka Spark Streaming")
 
 # Available Topics: 'Twitter_Blue_Tick', 'Russia_Ukrain_War', 'Climate Change', 'Elon Musk', 'Football','Cricket'
 
-# Loading the word2vec model
-#wv_path="home/ubuntu/Tstreaming/models/word2vec.model"
-#model=Word2VecModel.load(wv_path)
 
-# Loading the decision tree classifier model
-#dt_path="home/ubuntu/Tstreaming/models/Decision_Tree.Model"
-#dt=DecisionTreeClassificationModel.load(dt_path)
+from twitter import TWITTER_API
 
-def fun(x):
-    print(x)
+tweet=TWITTER_API
+
+querystring = {"list_id":"1591033111726391297","continuation_token":"HBaAgLydmczz0y0AAA==","limit":"100"}
+headers = {"X-RapidAPI-Key": "cffedc8ba0m7yftdrsrdfttretrtee4ertfgh1176dp14a29fjsn8f2e2b6580e6",
+                   "X-RapidAPI-Host": "twitter154.p.rapidapi.com"}
+max_count=5
+tweet=TWITTER_API()
+tweet.streaming(querystring,headers,max_count)
+print('Hello World')
+
+
 
 if __name__=="__main__":
-    fun()
-    print(fun(x))
-    
-    
-    
-    
-    
-    
-    
-"""
+    sc=SparkContext(appName="Kafka Spark Demo")
     ssc = StreamingContext(sc, 60)
     broker_list = 'localhost:9092'
     topic = 'random'
     message= KafkaUtils.createDirectStream(ssc,
                                 [topic],
                                 {"metadata.broker.list": broker_list})
-    
-    words=message.map(lambda x:x[1]) #.flatMap(lambda x:x.split())
-    #wordcount=words.map(lambda x:(x,1)).reduceByKey(lambda a,b:a+b)
-    words.pprint()
+    words=message.map(lambda x:x[1]).flatMap(lambda x:x.split())
+    wordcount=words.map(lambda x:(x,1)).reduceByKey(lambda a,b:a+b)
+    wordcount.ppprint()
     ssc.start()
     ssc.awaitTermination()
-
-
-
-
-
-"""
-
-
-
 
 
